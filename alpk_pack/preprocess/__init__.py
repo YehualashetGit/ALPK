657f18858e1521b847a8aae9584f041ad9f1d1e1
yaredtaddese <taddeseyared@gmail.com>
2017-05-29 19:54:08 +0300

    Implement normalize function

--- alpk_pack/preprocess/__init__.py
+++ alpk_pack/preprocess/__init__.py
@@ -2,6 +2,7 @@ from alpk_pack.preprocess.to_CV import to_CV
 from alpk_pack.preprocess import Regexpp
 from alpk_pack.preprocess import punkt
 
+normalize_rules_dir = "corpus/replace_rules/"
 
 
 def sent_tokenize(text):
@@ -18,5 +19,25 @@ def word_tokenize(text):
     words = punkt_word_tokenizer.word_tokenize(text)
     return words
     
-def normalize(text):
-	pass
+def normalize(text, removeEnglish = True, similarizePunctuations = True, similarizePhonetics = True, noAbbreviations = True):
+    """
+    Normalize given text by removing english, similarizing the same phone to same character
+    and changing abbreviations to their normal form
+    """
+    if (removeEnglish):
+        englishRemovalDict = getRegexDict(normalize_rules_dir + "english_removal.json")
+        text = replace(text, englishRemovalDict)
+
+    if (similarizePhonetics):
+        punktSimilarizationDict = getRegexDict(normalize_rules_dir + "punkt_character_similarization.json")
+        text = replace(text, punktSimilarizationDict)
+    
+    if (similarizePhonetics):
+        phoneSimilarizationDict = getRegexDict(normalize_rules_dir + "phone_character_similarization.json")
+        text = replace(text, phoneSimilarizationDict)
+    
+    if(noAbbreviations):
+        abbreviationsDict = getRegexDict(normalize_rules_dir + "abbreviations.json")
+        text = replace(text, abbreviationsDict)
+
+    return text
