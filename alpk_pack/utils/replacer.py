20e56c76497d9413190cc8969cef3ef2263013b1
yaredtaddese <taddeseyared@gmail.com>
2017-05-29 19:04:29 +0300

    Added replacer.py in utils in order to provide string replacing capability based on regex patterns

---
+++ alpk_pack/utils/replacer.py
@@ -0,0 +1,31 @@
+import re
+import json
+
+def getRegexDict(file_name):
+    regex_dictionary={}
+
+    with open(file_name) as json_file:
+        json_dict=json.load(json_file)
+        for key, value in json_dict.items():
+            regex=""
+            for token in value:
+                regex += str(token)
+                regex += "|"
+            regex = regex[:-1]
+            
+            regex_dictionary[key]=regex
+    return regex_dictionary
+
+def replace(text, regexDict):
+    """
+    For all keys in regexDict dictionary substitute regexDict key string when its corresponding 
+    value(which is regex pattern) is found in the text    
+    """
+    for key in regexDict:
+        if key == "empty":
+            print(key,": ", regexDict[key])
+            text = re.sub(regexDict[key], "", text)
+        else:
+            text = re.sub(regexDict[key], key, text)
+
+    return text
